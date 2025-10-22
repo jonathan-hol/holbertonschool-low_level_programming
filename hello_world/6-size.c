6fb179e9459eacc2e148173d094c73c37374994c
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-22 14:09:09 +0200

    o

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,8 +4,8 @@ int main(void)
 {
     printf("Size of a char: 1 byte(s)\n");
     printf("Size of an int: 4 byte(s)\n");
-    printf("Size of a long int: 4 byte(s)\n");
-    printf("Size of a long long int: 8 byte(s)\n");  // Remplacer par long long int pour garantir 8 octets
+    printf("Size of a long int: 4 byte(s)\n"); // Affiche la taille 8 octets pour long int (en 64 bits)
+    printf("Size of a long long int: 8 byte(s)\n");  
     printf("Size of a float: 4 byte(s)\n");
 
     return 0;
