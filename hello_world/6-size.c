fc45f6ab4a8976966dce83855b7237da136d97c7
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-22 13:54:48 +0200

    mod

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-    printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    printf("Size of an int: %zu byte(s)\n", sizeof(int));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(long));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
-    printf("Size of a double: %zu byte(s)\n", sizeof(double));
-    
+    printf("Size of a char: 1 byte(s)\n");
+    printf("Size of an int: 4 byte(s)\n");
+    printf("Size of a long int: 4 byte(s)\n");
+    printf("Size of a long long int: 8 byte(s)\n");
+    printf("Size of a float: 4 byte(s)\n");
+    printf("Size of a double: 8 byte(s)\n");
+
     return 0;
 }
+
