0f92f2d5a9291223e05039b86a5cef2a2269c182
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-22 13:51:05 +0200

    modi

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,15 +2,12 @@
 
 int main(void)
 {
-    // Affichage des tailles des types avec les noms exacts comme dans l'exemple
     printf("Size of a char: %zu byte(s)\n", sizeof(char));
     printf("Size of an int: %zu byte(s)\n", sizeof(int));
     printf("Size of a long int: %zu byte(s)\n", sizeof(long));
     printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
     printf("Size of a float: %zu byte(s)\n", sizeof(float));
-    // L'architecture de la machine peut changer la taille de long double
     printf("Size of a double: %zu byte(s)\n", sizeof(double));
-    printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
     
     return 0;
 }
