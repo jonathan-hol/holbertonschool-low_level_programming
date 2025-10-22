fc2fe19b008652fd39a8a30aa077cefdf91c9556
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-22 14:11:52 +0200

    m

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("Size of a char: 1 byte(s)\n");
-    printf("Size of an int: 4 byte(s)\n");
-    printf("Size of a long int: 4 byte(s)\n"); // Affiche la taille 8 octets pour long int (en 64 bits)
-    printf("Size of a long long int: 8 byte(s)\n");  
-    printf("Size of a float: 4 byte(s)\n");
-
-    return 0;
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
-
