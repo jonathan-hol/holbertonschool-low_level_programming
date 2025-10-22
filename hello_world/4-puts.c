e9353420888c6e2b136b8ee049b281f5628f2aac
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-22 10:40:15 +0200

    4 modi

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
