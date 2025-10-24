6c43c1ade11a463ea78fa5a8966d53ddee11bf66
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-24 18:37:02 +0200

    alphabet complet de az et AZ

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main Entry - point
- * 
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char lettre;
-    char lettreM;
-    
-    for (lettre = 'a'; lettre <= 'z'; lettre++)
-    {
-        putchar(lettre);
-    }
+	char lettre;
+	char lettreM;
 
-    for (lettreM = 'A'; lettreM <= 'Z'; lettreM++)
-    {
-        putchar(lettreM);
-    }
+	/* Afficher les lettres minuscules */
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+	{
+		putchar(lettre);
+	}
 
-    putchar('\n');
-    return (0);
+	/* Afficher les lettres majuscules */
+	for (lettreM = 'A'; lettreM <= 'Z'; lettreM++)
+	{
+		putchar(lettreM);
+	}
+
+	putchar('\n'); /* Ajouter un saut de ligne après l'affichage */
+	return (0);    /* Retourner 0 pour indiquer une exécution réussie */
 }
