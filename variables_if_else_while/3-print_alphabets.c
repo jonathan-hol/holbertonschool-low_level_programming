c8e98d9cdbc5f5722f252dd1e3cf3e72fdb2b01a
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-24 15:38:58 +0200

    lette m M

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main Entry - point
+ * 
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+    char lettre;
+    char lettreM;
+    
+    for (lettre = 'a'; lettre <= 'z'; lettre++)
+    {
+    putchar(lettre);
+    }
+    for (lettreM = 'A'; lettreM <= 'Z'; lettreM++)
+    {
+        putchar(lettreM);
+    }
+    putchar('\n');
+    return (0);
+}
