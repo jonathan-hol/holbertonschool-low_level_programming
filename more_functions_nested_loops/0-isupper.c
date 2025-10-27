9a53ca8a2119f7d48210f3cd02925512d41213dc
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-27 14:40:19 +0100

    modfi

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
-#include "main.h"
+#include "main.h"  /* Inclure main.h pour déclarer _isupper */
 
+/* Définition de la fonction _isupper */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z') {
-        return 1;
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);  /* C'est une majuscule */
     }
-    return (0);
+    return (0);  /* Ce n'est pas une majuscule */
 }
