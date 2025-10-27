5783b154de5d392b248576c130c296c69f552cf0
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-10-27 14:06:46 +0100

    check com betty

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
-#include "main.h"  /* Inclure main.h pour déclarer _isupper */
+#include "main.h"
 
-/* Définition de la fonction _isupper */
 int _isupper(int c)
 {
     if (c >= 'A' && c <= 'Z') {
-        return 1;  /* C'est une majuscule */
+        return 1;
     }
-    return 0;  /* Ce n'est pas une majuscule */
+    return 0;
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Déclaration de la fonction _isupper */
 int _isupper(int c);
 
-#endif /* MAIN_H */
+#endif
