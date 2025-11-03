5645b66fe713432a09f12112375574284aa65342
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-11-03 09:53:49 +0100

    alphabet sauf 'e' et 'q'

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char letter;
 
-    for (letter = 'a'; letter <= 'a'; letter++)
+    for (letter = 'a'; letter <= 'z'; letter++)
 {
     if (letter != 'e' && letter !='q')
 }
