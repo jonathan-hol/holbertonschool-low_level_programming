988fbf3c9b145c218b3507b90753e34db3b139f0
jonathan-hol <lebourdaisjonathan@gmail.com>
2025-11-03 09:46:12 +0100

    alphabet sauf 'e' et 'q'

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/** 
+* main - Entry point
+*
+* imprime l'alphabet en min sauf 'e' et 'q' en utilisant putchar
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'a'; letter++)
+{
+    if (letter != 'e' && letter !='q')
+    {
+        putchar (letter);
+    }
+}
+    putchar ('\n');
+
+    Return (0);
+
+}
